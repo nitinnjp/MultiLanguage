7f2eaac31d2f28dc4f690fd2cce93ced21aec1c0
Neosoft <Neosoft@DESKTOP-44DAAQS>
2021-01-05 16:08:37 +0530

    Add project files.

---
+++ MultiLanguage/Controllers/LanguageController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Globalization;
+
+namespace MultiLanguage.Controllers
+{
+    public class LanguageController : Controller
+    {
+        // GET: Language
+        public ActionResult Index(string languge)
+        {
+            if(!string.IsNullOrEmpty(languge))
+            {
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(languge);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(languge);
+            }
+            HttpCookie cookie = new HttpCookie("Languages");
+            cookie.Value = languge;
+            Response.Cookies.Add(cookie);
+            return View();
+        }
+    }
+}
